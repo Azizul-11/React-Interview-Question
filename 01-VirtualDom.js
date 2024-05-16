98f8e1110138278ee44747abc9ef7988cef61c25
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-05-16 13:31:37 +0530

    Add files via upload

---
+++ 01-VirtualDom.js
@@ -0,0 +1,11 @@
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+
+
+// createRoote-> Create a DOM like structure
+
+// go to this document for more about fiber and reconciliation
+// https://github.com/acdlite/react-fiber-architecture
+
